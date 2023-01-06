d14f24ed357c31536eb70cb76c52aaaab0db94b4
Jonas Pauli <chef@macbook-air.home>
2023-01-06 16:29:40 +0100

    small commit

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 from core.transfer import Transfer
 from core.blockchain import Block, Blockchain
 from core.accounts import Keys
-import time
+import time, os
 from chainspec import BLOCKTIME
+
+if not os.path.exists('./txpool'):
+    os.mkdir('./txpool')
 # 1. initialize a new, empty Blockchain instance
 Instance = Blockchain()
 Instance.new()
@@ -22,6 +25,7 @@ prev_Block_Dict = Instance.chain[0] # Genesis Block
 prev_Block = Block(prev_Block_Dict['index'], prev_Block_Dict['timestamp'], prev_Block_Dict['next_timestamp'], prev_Block_Dict['block_hash'], prev_Block_Dict['next_hash'], prev_Block_Dict['prev_hash'], prev_Block_Dict['transfers'])
 new_Block = Block(None, None, None, None, None, None, [])
 new_Block.new(prev_Block)
+# Write Block to Chain and delete temp file from txpool
 Instance.add_finalized_block(new_Block.finalize())
 print(Instance.chain)
 print("Transaction valid: ", tx.validate())
